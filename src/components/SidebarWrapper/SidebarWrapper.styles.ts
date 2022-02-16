fb50ef419eff68bea53cde97d1495d3daf3b4d49
Uryn Dmytro <dmytro@digia.co>
2022-02-16 22:07:49 +0200

    add border radius for sidebar wrapper

--- src/components/SidebarWrapper/SidebarWrapper.styles.ts
+++ src/components/SidebarWrapper/SidebarWrapper.styles.ts
@@ -3,6 +3,7 @@ import Box from '@mui/material/Box';
 
 export const SidebarWrapper = styled(Box)`
 	background: ${({ theme }) => theme.palette.secondary.main};
+  border-radius: 5px;
 	width: 100%;
 	height: 100%;
 `;
